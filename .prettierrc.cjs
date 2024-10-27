16d8472983ca7334346abe0cbf47b8f8bb74f042
KevOneRedOne <kevinalves@hotmail.fr>
2024-10-27 23:17:42 +0100

    Updates

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  proseWrap: 'always',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  htmlWhitespaceSensitivity: 'strict',
+  insertPragma: false,
+  requirePragma: false,
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: 'auto',
+};
